e3a3bc39746377287f3419dc647b7bf1c01d4a5f
Karim Abdul <karim@warpdevelopment.com>
2023-04-13 21:06:40 +0200

    cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,32 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducer';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-
+    cart: cartReducer
 });
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null;
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    : {};
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage,
+        //shippingAddress: shippingAddressFromStorage,
+    }
+}
 
 const middleware = [thunk];
 
